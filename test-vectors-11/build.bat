0e1f4b9a06a8f157eea810e54414c5f62ebdf53a
Marek Serafin <marek@snowheads.pl>
2022-03-16 15:57:48 +0100

    Ed25519 branch

--- test-vectors-11/build.bat
+++ test-vectors-11/build.bat
@@ -1,1 +1,1 @@
-g++ aes.c aes-ccm.c aes-enc.c vectors.cpp -lsodium -o vectors.exe -Ilibsodium-win64\include -Llibsodium-win64\lib -std=c++17
+g++ aes.c aes-ccm.c aes-enc.c vectors.cpp -lsodium -o vectors.exec -Ilibsodium/src/libsodium/include -Llibsodium/src -std=c++17
